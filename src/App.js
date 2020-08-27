940265e38fcfa6fdbfb49c2f5fcd7c04ff2a320c
Luyu Zeng <zengluyu252@gmail.com>
2020-08-27 18:10:13 -0400

    Updated employees.json

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Infoheader from "./components/Infoheader";
 import Employee from "./components/Employee";
+import Employees from "./Employees.json"
 
+const employeeArray = Employees.map((employee) => <Employee name={employee.name} image={employee.image} phone={employee.phone} dob={employee.dob} email={employee.email} />);
 function App() {
   return (
     <div>
       <Header />
       <Navbar />
       <Infoheader />
-      <Employee />
+      {employeeArray}
     </div>
   );
 }
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,29 +1,30 @@
 import React from "react";
+// import Employees from "../Employees.json"
 import "../styles/Employee.css";
 
-const employeeArray=[{image: "placeholder" , name: "Luyu", phone: "123-456-7891", email:"123@good.com" , dob:"08/26/2020",}]
+// const employeeArray=[{image: "placeholder" , name: "Luyu", phone: "123-456-7891", email:"123@good.com" , dob:"08/26/2020",}]
 
-function Employee() { 
+function Employee(props) { 
     return (
         <div className="container">
             <hr />
-            <div className="row">
-                <div className="col-md">
-                {employeeArray[0].image}
+                <div className="row">
+                    <div className="col-md">
+                        <img alt={props.name} src={props.image} className="img-thumbnail"/>
+                    </div>
+                    <div className="col-md">
+                        {props.name}
+                    </div>
+                    <div className="col-md">
+                    {props.phone}
+                    </div>
+                    <div className="col-md">
+                        {props.email}
+                    </div>
+                    <div className="col-md">
+                        {props.dob}
+                    </div>
                 </div>
-                <div className="col-md">
-                {employeeArray[0].name}
-                </div>
-                <div className="col-md">
-                {employeeArray[0].phone}
-                </div>
-                <div className="col-md">
-                {employeeArray[0].email}
-                </div>
-                <div className="col-md">
-                {employeeArray[0].dob}
-                </div>
-            </div>
         </div>
     );
  }
